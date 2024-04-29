d7f41e946f3b5caac654f91f1d80720a7136ac0e
yyypearl <hor629@naver.com>
2024-04-30 02:37:44 +0900

    chore(#3): PWA 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import withPlugins from "next-compose-plugins";
+import withPWA from "next-pwa";
+import typescript from "next-plugin-graphql";
+// Import other plugins as needed
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: "public",
+        },
+      },
+    ],
+    [
+      typescript,
+      {
+        typescriptLoaderOptions: {
+          transpileOnly: false,
+        },
+      },
+    ],
+    // Add other plugins as needed
+  ],
+  nextConfig
+);
