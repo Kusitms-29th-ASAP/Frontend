f4a3d37df20ae976a79ed3742c52d0e8f05db8e5
yyypearl <hor629@naver.com>
2024-05-01 18:08:09 +0900

    feat(#5): Tabbar 컴포넌트

--- src/stories/Tabbar.stories.tsx
+++ src/stories/Tabbar.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
-import Tabbar from "./Tabbar";
+import Tabbar from "@/components/common/Tabbar";
 
 export default {
   component: Tabbar,
