4da9ac8d6e94d418a4e8e4438e177e22a1e5f3f5
parkyejin <uiop5809@naver.com>
2024-05-04 00:13:15 +0900

    design(#8): 회원가입 시작 UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
 "use client";
 
 import styled from "styled-components";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return <Layout>메인페이지</Layout>;
+  const router = useRouter();
+
+  const handleKakaoLoginClick = () => {
+    router.push("/signin/terms");
+  };
+
+  return (
+    <Container>
+      <SociclLoginBox onClick={handleKakaoLoginClick}>
+        <Image
+          src="/assets/main/kakao_social.svg"
+          alt="kakao"
+          layout="fill"
+          objectFit="contain"
+        />
+      </SociclLoginBox>
+    </Container>
+  );
 }
 
-const Layout = styled.div``;
+const Container = styled.div`
+  padding: 20px;
+  background-image: url("/assets/main/main_background.svg");
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 480px;
+  height: 100vh;
+  position: relative;
+`;
+
+const SociclLoginBox = styled.div`
+  width: 100%;
+  height: 48px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 10%;
+  cursor: pointer;
+`;
