65a66d4ebb2439947fc3417437566bcf021df474
parkyejin <uiop5809@naver.com>
2024-05-08 21:24:03 +0900

    docs(#6): 카카오 소셜 로그인 URI 변경

--- src/apis/auth/postKakaoToken.tsx
+++ src/apis/auth/postKakaoToken.tsx
@@ -1,7 +1,7 @@
 import Axios from "../axios";
 
 export async function postKakaoToken(accessToken: string): Promise<void> {
-  await Axios.post("/api/v1/auth/kakao", accessToken);
+  await Axios.post("/api/v1/auth/KAKAO", accessToken);
 }
 
 export default postKakaoToken;
