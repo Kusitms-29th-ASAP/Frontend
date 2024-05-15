ef4cebb4fcc316e240c828527d0e4f3d69a16922
yyypearl <hor629@naver.com>
2024-05-16 03:26:35 +0900

    design(#34): 마이페이지 - 서류제출, 자녀관리 page 추가

---
+++ src/app/mypage/document/layout.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+
+const layout = (props: any) => {
+  return <Container>{props.children}</Container>;
+};
+
+export default layout;
+
+const Container = styled.div`
+  width: 100%;
+`;
