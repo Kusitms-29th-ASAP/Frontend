3ee257758a0e1e4e47d18f1f5745656606eaf300
parkyejin <uiop5809@naver.com>
2024-05-17 02:30:22 +0900

    design(#29): 현장체험학습 신청서 작성

--- src/app/school/parentReference/workStudy/form/page.tsx
+++ src/app/school/parentReference/workStudy/form/page.tsx
@@ -1,9 +1,66 @@
+"use client";
+
+import Button from "@/components/common/Button";
+import Topbar from "@/components/common/Topbar";
+import styled from "styled-components";
+import Image from "next/image";
+
 const WorkStudyForm = () => {
   return (
-    <div>
-      <h1>WorkStudy Reason Form</h1>
-    </div>
+    <Container>
+      <div>
+        <Topbar text="체험학습 신청서 작성" icon={true} />
+        <Title>
+          <span>체험학습 신청서를 클릭</span>해서 <br />
+          작성을 시작해보세요
+        </Title>
+      </div>
+      <ImageBox style={{ width: "90%" }}>
+        <Image
+          src="/assets/school/workstudy_form.svg"
+          alt="workstudy_form"
+          layout="fill"
+          objectFit="contain"
+        />
+      </ImageBox>
+      <Button type={"gray"} text={"제출하기"} />
+    </Container>
   );
 };
 
 export default WorkStudyForm;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px 20px;
+  height: 100vh;
+  div {
+    width: 100%;
+  }
+`;
+
+const Title = styled.div`
+  margin: 16px 0;
+  ${({ theme }) => theme.fonts.heading2_b};
+  color: ${({ theme }) => theme.colors.b700};
+  span {
+    color: ${({ theme }) => theme.colors.primary500};
+  }
+`;
+
+const ImageBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+
+  img {
+    width: 80%;
+    position: relative !important;
+    object-fit: cover;
+  }
+`;
