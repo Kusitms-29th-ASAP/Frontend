9081456b56aed804605643451d677bd6248dbf81
yyypearl <hor629@naver.com>
2024-05-18 02:23:07 +0900

    fix(#40): 소식 상단 탭바 오류 수정

--- src/data/commonData.ts
+++ src/data/commonData.ts
@@ -1,7 +1,7 @@
 export const tabs = [
     { path: "/home", icon: "ic_home", label: "홈" },
     { path: "/school", icon: "ic_edu", label: "학교 생활" },
-    { path: "/news/school", icon: "ic_class", label: "소식" },
+    { path: "/news/school", subPath: "/news/eduOffice", icon: "ic_class", label: "소식" },
     { path: "/study", icon: "ic_study", label: "가정학습" },
     { path: "/mypage", icon: "ic_mypage", label: "마이페이지" },
   ];
