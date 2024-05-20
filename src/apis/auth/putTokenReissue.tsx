d5f34d8bea6f819e0091feed4172d90a1415d035
parkyejin <uiop5809@naver.com>
2024-05-21 02:36:24 +0900

    feat(#26): 토큰 재발급 API 연동 로직 수정

--- src/apis/auth/putTokenReissue.tsx
+++ src/apis/auth/putTokenReissue.tsx
@@ -1,9 +1,17 @@
+import { TokenResponse } from "@/interface/Auth";
 import Axios from "../axios";
 
-export async function putTokenReissue(refreshToken: string): Promise<void> {
-  await Axios.put("/api/v1/auth/reissue", {
-    refreshToken: refreshToken,
-  });
+export async function putTokenReissue(
+  refreshToken: string
+): Promise<TokenResponse> {
+  try {
+    const response = await Axios.put<TokenResponse>("/api/v1/auth/reissue", {
+      refreshToken: refreshToken,
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Failed to reissue token: " + error);
+  }
 }
 
 export default putTokenReissue;
