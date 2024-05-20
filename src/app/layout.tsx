ed74316587a6e7aa7ab3dd72b8fdd19777985cad
parkyejin <uiop5809@naver.com>
2024-05-20 15:42:29 +0900

    fix(#22): 모바일 웹에서 input 클릭 시 화면 확대 현상

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,20 @@ import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import "./global.css";
 import NOSSR from "@/components/common/NOSSR";
+import { Metadata } from "next";
 
 const queryClient = new QueryClient();
+
+export const metadata: Metadata = {
+  title: "스쿨포인트",
+  description: "스쿨포인트는 학생들을 위한 학습 관리 서비스입니다.",
+  keywords: ["스쿨포인트", "학습 관리", "학생"],
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+};
 
 export default function RootLayout({
   children,
