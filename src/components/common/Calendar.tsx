9d05f98fd3f22e44c86325ad2ae4ff491dcb899e
yyypearl <hor629@naver.com>
2024-05-23 02:48:13 +0900

    fix(#80): 캘린더 오류 수정

--- src/components/common/Calendar.tsx
+++ src/components/common/Calendar.tsx
@@ -2,23 +2,22 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { MobileDatePicker } from "@mui/x-date-pickers";
+import { styled } from "@mui/material/styles";
 import { useState } from "react";
 import CustomInput from "./CustomInput";
 import Image from "next/image";
+import { styled as styledComponents } from "styled-components";
 import dayjs from "dayjs";
-import styled from "styled-components";
 
 export interface CalendarProps {
   value: string;
   onChange: (value: string) => void;
   color?: "black";
   disabled?: boolean;
-  placeholder?: string;
 }
 
-const Calendar = (props: CalendarProps) => {
+const Calendar = ({ value, onChange, color, disabled }: CalendarProps) => {
   const [date, setDate] = useState<dayjs.Dayjs | null>(null);
-  const { value, onChange, disabled, placeholder } = props;
 
   const handleDateChange = (newDate: dayjs.Dayjs | null) => {
     setDate(newDate);
@@ -37,7 +36,7 @@ const Calendar = (props: CalendarProps) => {
           <CustomInputContainer>
             <CalendarInput
               value={value}
-              placeholder={placeholder ? placeholder : "날짜를 선택해주세요"}
+              placeholder="날짜를 선택해주세요"
               onChange={() => {}}
               readonly={true}
               color="black"
@@ -65,36 +64,35 @@ const Calendar = (props: CalendarProps) => {
 
 export default Calendar;
 
-const StyledDemoContainer = styled(DemoContainer)`
-  width: 100%;
+const StyledDemoContainer = styled(DemoContainer)({
+  width: "100%",
+  position: "relative",
+});
+
+const CustomInputContainer = styledComponents.div`
   position: relative;
-`;
-
-const CustomInputContainer = styled.div`
-  position: relative;
   width: 100%;
 `;
 
-const StyledMobileDatePicker = styled(MobileDatePicker)`
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  opacity: 0;
-  & .MuiInputBase-root {
-    height: 44px;
-  }
-`;
+const StyledMobileDatePicker = styled(MobileDatePicker)({
+  width: "100%",
+  position: "absolute",
+  top: "0",
+  left: "0",
+  opacity: "0",
+  "& .MuiInputBase-root": { height: "44px" },
+});
 
-const CalendarInput = styled(CustomInput)`
-  width: 440px;
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
+const CalendarInput = styled(CustomInput)({
+  width: "440px",
+  position: "absolute",
+  top: "0",
+  left: "0",
+  cursor: "pointer",
+});
 
-const IconImage = styled(Image)`
-  position: absolute;
-  top: 12px;
-  right: 15px;
-`;
+const IconImage = styled(Image)({
+  position: "absolute",
+  top: "12px",
+  right: "15px",
+});
