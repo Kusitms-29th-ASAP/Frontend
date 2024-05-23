ea2a76174d7cf706b02fbfe2e6fee36716cbcf15
parkyejin <uiop5809@naver.com>
2024-05-23 18:19:20 +0900

    docs(#96): 회신이 완료된 가정통신문 내역

--- src/data/notifyData.ts
+++ src/data/notifyData.ts
@@ -1,29 +1,52 @@
 export const notiCategoryData = [
-    {
-        id: "all",
-        category: "전체"
-    },
-    {
-        id: "notification",
-        category: "가정통신문"
-    },
-    {
-        id: "study",
-        category: "학습정보"
-    },
+  {
+    id: "all",
+    category: "전체",
+  },
+  {
+    id: "notification",
+    category: "가정통신문",
+  },
+  {
+    id: "study",
+    category: "학습정보",
+  },
 ];
 
 export const notiCardData = [
-    {
-        category: "가정통신문",
-        id: "notification",
-        date: "2024년 05월 24일",
-        text: "5월 24일, 오늘의 가정통신문 1건을 확인해보세요!",
-    },
-    {
-        category: "학습정보",
-        id: "study",
-        date: "2024년 05월 24일",
-        text: "이번주 업로드 된 학습정보를 살펴보세요!",
-    },
-]
+  {
+    category: "가정통신문",
+    id: "notification",
+    date: "2024년 05월 24일",
+    text: "5월 24일, 오늘의 가정통신문 1건을 확인해보세요!",
+  },
+  {
+    category: "학습정보",
+    id: "study",
+    date: "2024년 05월 24일",
+    text: "이번주 업로드 된 학습정보를 살펴보세요!",
+  },
+];
+
+export const HomeGideReplyCompletedData = [
+  {
+    id: 1,
+    content1: "2024 서울진로직업박람회 공모전",
+    content2: "교내외 프로그램",
+  },
+  {
+    id: 2,
+    content1: "양원숲 토닥토닥 상담실 운영 안내",
+    content2: "교내외 프로그램",
+  },
+  {
+    id: 3,
+    content1: "씨앗부터 시작하는 생생레시피",
+    content2: "교내외 프로그램",
+  },
+  {
+    id: 4,
+    content1: "영재교육원 3~4월 모집 요강 안내",
+    content2: "교내외 프로그램",
+  },
+];
