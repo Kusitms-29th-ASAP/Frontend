e0cb284577c3a2044385183b496313cf47466004
yyypearl <hor629@naver.com>
2024-06-07 01:53:42 +0900

    docs(#112): 주간학습안내 더미데이터 수정

--- src/data/todayStudyData.ts
+++ src/data/todayStudyData.ts
@@ -2,314 +2,158 @@ export const TodayStudyData = [
   {
     id: 1,
     week: 1,
-    subject: "영어",
+    subject: "즐거운 생활",
     studies: [
       {
         id: 1,
-        title: "lesson 4: How many apples? - Conncect and Write",
-        textbook: "44-45",
+        title: "마을 - 마을 탐험을 떠나요",
+        textbook: "26-27",
       },
     ],
   },
   {
     id: 2,
     week: 1,
-    subject: "수학",
+    subject: "국어",
     studies: [
       {
         id: 2,
-        title: "6단원 분수와 소수 - 소수를 알아볼까요",
-        textbook: "126-127",
+        title: "3. 겪은 일을 나타내요 - 꾸며 주는 말이 들어간 문장 읽기",
+        textbook: "80-83",
       },
     ],
   },
   {
     id: 3,
     week: 1,
-    subject: "체육",
+    subject: "수학",
     studies: [
       {
         id: 4,
-        title: "영양 교육 - 올바른 식품 선택 방법 알아보기",
+        title: "5. 분류하기 - 기준에 따라 분류해 볼까요",
+        textbook: "122-125"
       },
     ],
   },
   {
     id: 4,
     week: 1,
-    subject: "사회",
+    subject: "바른 생활",
     studies: [
       {
         id: 4,
         title:
-          "1단원 우리 고장의 옛 이야기 - 옛 이야기로 우리 고장에 관해 무엇을 알 수 있을까요?",
-        textbook: "56-57",
-      },
-    ],
-  },
-  {
-    id: 5,
-    week: 1,
-    subject: "국어",
-    studies: [
-      {
-        id: 4,
-        title: "5단원 중요한 내용을 적어요 - 글을 읽고 내용 간추리기",
-        textbook: "144-150",
-        material: "국어활동",
+          "마을 - 마을을 위해서 해 봐요",
+        textbook: "46-47",
       },
     ],
   },
   {
     id: 1,
     week: 2,
-    subject: "사회",
+    subject: "국어",
     studies: [
       {
         id: 1,
-        title: "1단원 우리 고장의 문화유산 - 문화재 안내도 만들기",
+        title: "3. 겪은 일을 나타내요 - 겪은 일에서 일기 글감 정하기",
+        textbook: "94-100"
       },
     ],
   },
   {
     id: 2,
     week: 2,
-    subject: "사회",
+    subject: "수학",
     studies: [
       {
         id: 2,
-        title: "1단원 우리 고장의 문화유산 - 출발! 양원숲 탐험대!",
+        title: " 5. 분류하기 - 수학이랑 확인해요",
+        textbook: "132-133"
       },
     ],
   },
   {
     id: 3,
     week: 2,
-    subject: "영어",
+    subject: "즐거운 생활",
     studies: [
       {
         id: 4,
-        title: "lesson 7: What is the weather today? - be 동사와 날씨 표현",
-        textbook: "46-48",
+        title: "세계 - 세계의 놀이",
+        textbook: "42-43",
       },
     ],
   },
   {
     id: 4,
     week: 2,
-    subject: "체육",
+    subject: "바른 생활",
     studies: [
       {
         id: 4,
-        title: "2단원 운동과 체력 - 줄넘기로 체력 기르기",
-        textbook: "22-23",
-      },
-    ],
-  },
-  {
-    id: 5,
-    week: 2,
-    subject: "과학",
-    studies: [
-      {
-        id: 4,
-        title: "2단원 동물의 한살이 - 알을 낳는 동물의 한살이",
-        textbook: "60-67",
-      },
-    ],
-  },
-  {
-    id: 6,
-    week: 2,
-    subject: "국어",
-    studies: [
-      {
-        id: 4,
-        title: "5단원 중요한 내용을 적어요 - 글을 읽고 내용 간추리기",
-        textbook: "144-150",
-        material: "국어활동",
+        title: "세계 - 안전하게 이동해요",
+        textbook: "76-77",
       },
     ],
   },
   {
     id: 1,
     week: 3,
-    subject: "도덕",
+    subject: "국어",
     studies: [
       {
         id: 1,
         title:
-          "2단원 인내하며 최선을 다하는 생할 - 인내하며 최선을 다하기 위해 꾸준히 노력해요",
-        textbook: "36-37",
+          "4. 분위기를 살려 읽어요 - 겹받침이 있는 낱말 읽고 쓰기",
+        textbook: "118-122",
       },
     ],
   },
   {
     id: 2,
     week: 3,
-    subject: "수학",
+    subject: "국어",
     studies: [
       {
         id: 2,
-        title: "6단원 분수와 소수 - 소수를 알아볼까요",
-        textbook: "128-129",
+        title: "4. 분위기를 살려 읽어요 - 시의 분위기 살펴보기",
+        textbook: "128-132",
       },
     ],
   },
   {
     id: 3,
     week: 3,
-    subject: "자율",
+    subject: "수학",
     studies: [
       {
         id: 4,
-        title: "행복배움 예체능교육 - 리코더 교육",
+        title: " 6. 곱셈 - 묶어 세어 볼까요",
+        textbook: "142-143"
       },
     ],
   },
   {
     id: 4,
     week: 3,
-    subject: "자율",
+    subject: "즐거운 생활",
     studies: [
       {
         id: 4,
-        title: "행복배움 디지털 교육 - 코딩교육",
+        title: "세계 - 세계의 장난감",
+        textbook: "44-45"
       },
     ],
   },
   {
     id: 5,
     week: 3,
-    subject: "국어",
+    subject: "슬기로운 생활",
     studies: [
       {
         id: 4,
-        title: "5단원 중요한 내용을 적어요 - 글을 읽고 내용 간추리기",
-        textbook: "151-155",
-        material: "국어활동",
-      },
-    ],
-  },
-  {
-    id: 1,
-    week: 4,
-    subject: "음악",
-    studies: [
-      {
-        id: 1,
-        title: "2단원 느낌을 담아서 - 높은음 낮은음",
-        textbook: "133-135",
-      },
-    ],
-  },
-  {
-    id: 2,
-    week: 4,
-    subject: "미술",
-    studies: [
-      {
-        id: 2,
-        title: "1단원 관찰하고 표현해요 - 자세히 보고 표현하기",
-        textbook: "30-31",
-      },
-    ],
-  },
-  {
-    id: 3,
-    week: 4,
-    subject: "미술",
-    studies: [
-      {
-        id: 4,
-        title: "1단원 관찰하고 표현해요 - 자세히 보고 표현하기",
-        textbook: "30-31",
-      },
-    ],
-  },
-  {
-    id: 4,
-    week: 4,
-    subject: "수학",
-    studies: [
-      {
-        id: 4,
-        title: "6단원 분수와 소수 - 소수를 알아볼까요",
-        textbook: "130-132",
-      },
-    ],
-  },
-  {
-    id: 5,
-    week: 4,
-    subject: "과학",
-    studies: [
-      {
-        id: 4,
-        title: "2단원 동물의 한살이 - 새끼를 낳는 동물의 한살이",
-        textbook: "151-155",
-      },
-    ],
-  },
-  {
-    id: 1,
-    week: 5,
-    subject: "수학",
-    studies: [
-      {
-        id: 1,
-        title: "3단원 길이와 시간 - 시간 단위 개념을 이해해봅시다",
-        textbook: "133-135",
-      },
-    ],
-  },
-  {
-    id: 2,
-    week: 5,
-    subject: "음악",
-    studies: [
-      {
-        id: 2,
-        title: "2단원 느낌을 담아 - 리 자로 끝나는 말은",
-        textbook: "30-31",
-      },
-    ],
-  },
-  {
-    id: 3,
-    week: 5,
-    subject: "국어",
-    studies: [
-      {
-        id: 4,
-        title: "4단원 높임표현 배워봐요 - 존댓법 종류를 알아볼까요?",
-        textbook: "151-155",
-      },
-    ],
-  },
-  {
-    id: 4,
-    week: 5,
-    subject: "과학",
-    studies: [
-      {
-        id: 4,
-        title:
-          "3단원 서로 다른 물질의 성질 - 서로 다른 두 물질이 만나면 무슨 일이 일어날까요?",
-        textbook: "70-71",
-      },
-    ],
-  },
-  {
-    id: 5,
-    week: 5,
-    subject: "국어",
-    studies: [
-      {
-        id: 4,
-        title: "5단원 내 마음을 편지에 담아 - 편지 작성 예절을 알아볼까요?",
-        textbook: "156-157",
-        material: "국어활동",
+        title: "마을 - 우리 마을 전시회",
+        textbook: "54-55",
       },
     ],
   },
