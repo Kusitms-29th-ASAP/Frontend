e44d5eb9f2d5bc7a2744cd2160c3751e890ab414
yyypearl <hor629@naver.com>
2024-08-20 22:00:29 +0900

    design(#132): 마이페이지 언어 및 폰트 사이즈 환경설정 UI

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -3,8 +3,12 @@
 import deleteUser from "@/apis/auth/deleteUser";
 import Axios from "@/apis/axios";
 import getUserInfo from "@/apis/user/getUserInfo";
+import Checkbox from "@/components/common/Checkbox";
+import CustomInput from "@/components/common/CustomInput";
 import Tabbar from "@/components/common/Tabbar";
 import Topbar from "@/components/common/Topbar";
+import { FONT_SIZE } from "@/constants/font";
+import { LANGUAGE } from "@/constants/language";
 import { RootState } from "@/redux/store";
 import { theme } from "@/styles/theme";
 import Image from "next/image";
@@ -43,6 +47,10 @@ const Mypage = () => {
   const [userName, setUserName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
 
+  const [language, setLanguage] = useState<string>("");
+  const [fontSize, setFontSize] = useState<number>(FONT_SIZE[0].id);
+  const [openDropdown, setOpenDropdown] = useState(false);
+
   const userInfo = async () => {
     const data = await getUserInfo();
     setUserName(data.userName);
@@ -55,6 +63,29 @@ const Mypage = () => {
   }
   const newPhoneNumber = formatPhoneNumber(phoneNumber);
 
+  const handleTypeSelectClick = () => {
+    setOpenDropdown(!openDropdown);
+  };
+
+  const handleSelectDrop = (selectedDrop: string) => {
+    setLanguage(selectedDrop);
+    setOpenDropdown(false);
+
+    const selectedLanguage = LANGUAGE.find(
+      (lang) => lang.text === selectedDrop
+    );
+
+    if (selectedLanguage) {
+      localStorage.setItem("language", selectedLanguage.eng);
+    }
+  };
+
+  const handleCheckboxChange = (item: { id: number }) => {
+    setFontSize(item.id);
+
+    localStorage.setItem("fontSize", item.id.toString());
+  };
+
   const handleLogout = () => {
     deleteUser(refreshToken);
     router.push("/");
@@ -65,23 +96,43 @@ const Mypage = () => {
   useEffect(() => {
     if (childList.length > 0) {
       const primaryChild = childList.find((child) => child.isPrimary);
-      Axios.get(`/api/v1/children/${primaryChild?.childId}`)
-        .then((response) => {
+      Axios.get(`/api/v1/children/${primaryChild?.childId}`).then(
+        (response) => {
           const data = response.data;
           setChild(data);
-        })
-        .catch((error) => {});
+        }
+      );
     }
   }, [childList]);
 
   /* 자녀 전체 불러오기 */
   useEffect(() => {
-    Axios.get(`/api/v1/children`)
-      .then((response) => {
-        const data = response.data.childList;
-        setChildList(data);
-      })
-      .catch((error) => {});
+    Axios.get(`/api/v1/children`).then((response) => {
+      const data = response.data.childList;
+      setChildList(data);
+    });
+  }, []);
+
+  /* 언어 및 폰트 정보 불러오기 */
+  useEffect(() => {
+    const storedLanguageEng = localStorage.getItem("language");
+    if (storedLanguageEng) {
+      const matchingLanguage = LANGUAGE.find(
+        (lang) => lang.eng === storedLanguageEng
+      );
+      if (matchingLanguage) {
+        setLanguage(matchingLanguage.text);
+      }
+    } else {
+      localStorage.setItem("language", "한국어");
+    }
+
+    const storedFontSize = localStorage.getItem("fontSize");
+    if (storedFontSize) {
+      setFontSize(parseInt(storedFontSize));
+    } else {
+      localStorage.setItem("fontSize", 0 + "");
+    }
   }, []);
 
   return (
@@ -148,6 +199,49 @@ const Mypage = () => {
           </Line>
         </ColContainCard>
         <ColContainCard>
+          <Title>환경설정</Title>
+          <div>
+            <Label>언어</Label>
+            <Box>
+              <StyledInput
+                value={language}
+                onClick={handleTypeSelectClick}
+                onChange={() => {}}
+                placeholder="언어"
+                inputType="select"
+                clicked={openDropdown}
+              />
+              {openDropdown && (
+                <DropDown>
+                  {LANGUAGE.map((data) => (
+                    <Cusor
+                      key={data.id}
+                      onClick={() => handleSelectDrop(data.text)}
+                    >
+                      {data.text}
+                    </Cusor>
+                  ))}
+                </DropDown>
+              )}
+            </Box>
+          </div>
+          <div>
+            <Label>글자 크기</Label>
+            <CheckboxBox>
+              {FONT_SIZE.map((item) => (
+                <Checkbox
+                  key={item.id}
+                  label={item.label}
+                  checkboxType="checkBtn"
+                  checked={fontSize === item.id}
+                  onChange={() => handleCheckboxChange(item)}
+                  justifyContent="space-between"
+                />
+              ))}
+            </CheckboxBox>
+          </div>
+        </ColContainCard>
+        <ColContainCard>
           <LogOut onClick={handleLogout}>로그아웃</LogOut>
           <DeleteUser onClick={handleDeleteUser}>회원탈퇴</DeleteUser>
         </ColContainCard>
@@ -174,10 +268,11 @@ const Padding = styled.div`
 
 const Background = styled.div`
   height: 100%;
+  min-height: 820px;
   display: flex;
   flex-direction: column;
   gap: 20px;
-  padding: 20px 20px;
+  padding: 20px;
   background: ${theme.colors.b80};
 `;
 
@@ -252,6 +347,62 @@ const DarkGray = styled.div`
 
 const DarkGrayCap = styled(DarkGray)`
   ${(props) => props.theme.fonts.caption1_r};
+`;
+
+const Title = styled.div`
+  color: ${theme.colors.b700};
+  ${(props) => props.theme.fonts.body1_b};
+`;
+
+const Label = styled.div`
+  color: ${theme.colors.b700};
+  ${(props) => props.theme.fonts.body3_b};
+  margin-bottom: 5px;
+`;
+
+const Box = styled.div`
+  position: relative;
+`;
+
+const StyledInput = styled(CustomInput)`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const DropDown = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 44px;
+  left: 0;
+  padding: 16px 218px 16px 16px;
+  border-radius: 10px;
+  border: 1px solid ${theme.colors.b200};
+  background: ${theme.colors.white};
+  box-shadow: 0px 2px 64px 0px rgba(30, 41, 59, 0.06);
+  z-index: 1000;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+  color: ${theme.colors.b500};
+  ${(props) => props.theme.fonts.body3_b};
+`;
+
+const CheckboxBox = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(120px, 1fr));
+  gap: 8px;
+
+  @media screen and (max-width: 330px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(2, 1fr);
+  }
+`;
+
+const Cusor = styled.div`
+  white-space: nowrap;
+  cursor: pointer;
 `;
 
 const LogOut = styled.div`
---
+++ src/constants/font.ts
@@ -0,0 +1,12 @@
+export const FONT_SIZE = [
+    {
+        id: 0,
+        label: "기본",
+        size: 1,
+    },
+    {
+        id: 1,
+        label: "1.2배",
+        size: 1.2,
+    },
+];
